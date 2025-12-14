9f363e19c9af7f39909e7b9a72e9e9b97a68a101
Alena Lyalyushkina <pluto_16@mail.ru>
2025-12-14 15:11:58 +1000

    fix: wrap page.tsx with Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import BlogList from "@/components/BlogList/BlogList";
 import SearchForm from '@/components/SearchForm/SearchForm';
 import styles from './page.module.scss';
@@ -5,8 +6,10 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <SearchForm />
-      <BlogList />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <SearchForm />
+        <BlogList />
+      </Suspense>
     </div>
   );
 }
